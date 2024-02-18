be4d53a5c8b13b329f8cc4794bcc4f7b22cce680
InquisitorCodes <156965864+InquisitorCodes@users.noreply.github.com>
2024-02-18 14:35:15 -0400

    Add files via upload

--- CopiarPegar.js
+++ CopiarPegar.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("btn-copy").addEventListener("click", function() {
-      var textoACopiarInput = document.getElementById("input-txt").value; // Obtener el texto de la primera textarea
-      var textoACopiarMsg = document.getElementById("msg").value; // Obtener el texto de la segunda textarea
-      var textoACopiar = textoACopiarInput + "\n" + textoACopiarMsg; // Concatenar ambos textos
 
-      console.log("Texto a copiar:", textoACopiar); // Agregar este mensaje para depurar
-      copiarTextoAlPortapapeles(textoACopiar);
+   document.getElementById("btn-copy").addEventListener("click", function() {
+      let textoACopiarMsg = document.getElementById("msg").value;
+      copiarTextoAlPortapapeles(textoACopiarMsg);
+      
+  });
+
+    document.getElementById("btn-copy2").addEventListener("click", function() {
+      let textoACopiarInput = document.getElementById("input-txt").value; // Obtener el texto de la segunda textarea
+      copiarTextoAlPortapapeles(textoACopiarInput);
   });
 
   function copiarTextoAlPortapapeles(texto) {
@@ -15,6 +18,6 @@ document.addEventListener("DOMContentLoaded", function() {
       elementoTemp.select();
       document.execCommand("copy");
       document.body.removeChild(elementoTemp);
-      console.log("Texto copiado al portapapeles:", texto); // Agregar este mensaje para depurar
   }
+
 });
